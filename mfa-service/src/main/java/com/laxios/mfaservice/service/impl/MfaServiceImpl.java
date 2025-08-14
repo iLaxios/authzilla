72b9c25d3a7fb9b9c91a01794a252961cc6ed580
Laxman B <dev.laxmanb@gmail.com>
2025-08-14 08:39:26 +0000

    add jwt util

--- mfa-service/src/main/java/com/laxios/mfaservice/service/impl/MfaServiceImpl.java
+++ mfa-service/src/main/java/com/laxios/mfaservice/service/impl/MfaServiceImpl.java
@@ -2,11 +2,24 @@ package com.laxios.mfaservice.service.impl;
 
 import com.laxios.mfaservice.dto.SetupRequest;
 import com.laxios.mfaservice.service.MfaService;
+import com.laxios.mfaservice.util.JwtUtil;
+import lombok.Data;
 import org.springframework.stereotype.Service;
 
 @Service
+@Data
 public class MfaServiceImpl implements MfaService {
+
+    private final JwtUtil jwtUtil;
+
     public void setup(SetupRequest request) {
+
+        String token = request.getToken();
+
+        if(!jwtUtil.validateToken(token)) {
+            throw new IllegalArgumentException("Invalid token");
+        }
+        
         System.out.println(request.getToken());
     }
 }
---
+++ mfa-service/src/main/java/com/laxios/mfaservice/util/JwtUtil.java
@@ -0,0 +1,64 @@
+package com.laxios.mfaservice.util;
+
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+import java.util.UUID;
+
+@Component
+@Data
+@NoArgsConstructor
+public class JwtUtil {
+
+    @Value("${jwt.secret-key}")
+    private String secret;
+
+    @Value("${jwt.expiration-hr}")
+    private long expirationHr;
+
+    private Key key;
+
+    @PostConstruct
+    public void init() {
+        // Decode Base64 secret and build key
+        key = Keys.hmacShaKeyFor(Base64.getDecoder().decode(secret));
+    }
+
+    public String generateToken(UUID userId, String username) {
+        long expirationMs = expirationHr * 1000 * 60 * 60;
+        return Jwts.builder()
+                .setSubject(userId.toString())
+                .claim("username", username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expirationMs))
+                .signWith(key)
+                .compact();
+    }
+
+    public Boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (ExpiredJwtException e) {
+            System.out.println("Token expired");
+        } catch (JwtException e) {
+            System.out.println("Invalid token");
+        }
+        return false;
+    }
+}
