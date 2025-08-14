d7ab732009e19fba95f98c032943804b06687cf4
Laxman B <dev.laxmanb@gmail.com>
2025-08-14 07:33:05 +0000

    setup MFA service spring-boot

---
+++ mfa-service/src/main/java/com/laxios/mfaservice/MfaServiceApplication.java
@@ -0,0 +1,11 @@
+package com.laxios.mfaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfaServiceApplication {
+    public static void main(String args[]) {
+        SpringApplication.run(MfaServiceApplication.class);
+    }
+}
